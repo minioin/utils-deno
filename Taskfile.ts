56e2e413fe19da33293442d71a7a09c7426d4dff
Minioin <minioin@protonmail.com>
2021-08-23 14:22:18 -0400

    feat: add taskfile and taskutils

---
+++ Taskfile.ts
@@ -0,0 +1,28 @@
+import { cmd, run } from "./lib/taskutils.ts";
+
+export const fmt = run(
+  cmd("deno fmt ."),
+);
+
+export const lint = run(
+  cmd("deno lint ."),
+);
+
+export const test = run(
+  cmd("deno --unstable test"),
+);
+
+export const clean = run(
+  cmd(`rm -rf build/`),
+);
+
+export const roles = run(
+  cmd("deno run --unstable -A examples/ansible.ts"),
+);
+
+export const build = run(
+  fmt,
+  lint,
+  test,
+  roles,
+);
---
+++ lib/taskutils.ts
@@ -0,0 +1,38 @@
+import { $ } from "https://deno.land/x/deno_dx/mod.ts";
+
+export function defer(fn: (...args: unknown[]) => unknown) {
+  return (...args: unknown[]) => {
+    return () => {
+      fn(...args);
+    };
+  };
+}
+
+export function run(...args: (() => void)[]) {
+  return async () => {
+    for (const arg of args) {
+      try {
+        await arg();
+      } catch (e) {
+        console.error(`Error while running ${arg}`);
+        console.error(`Error: `, e);
+      }
+    }
+    return args;
+  };
+}
+
+export const log = defer((...args: unknown[]) => console.log(...args));
+export const kaf = (file: string) => () => $`kubectl apply -f ${file}`;
+export const kdf = (file: string) => () => $`kubectl delete -f ${file}`;
+
+export function cmd(cmd: string, silent = false) {
+  return () => {
+    try {
+      (silent || log(cmd)());
+      return $`${cmd}`;
+    } catch (e) {
+      console.error(`Error running: ${cmd}. ${JSON.stringify(e)}`);
+    }
+  };
+}
